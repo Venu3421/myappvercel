c274d6a96aae5051e72851c178f39c8b49b989f3
Venu3421 <venupaka006@gmail.com>
2026-02-27 14:43:48 +0530

    Fix merge conflict in App.js - use frontend branch routing setup

--- src/App.js
+++ src/App.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-=======
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Reg from "./Reg"
 import Home from "./Home"
@@ -42,5 +15,3 @@ function App() {
   )
 }
 export default App
-
->>>>>>> a6e2a2c (frontend)
